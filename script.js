a4409d5e7e4188a325dc8947854ca4bb70426716
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-04-25 07:33:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
 const divide = (arr, n) => {
-  // Write your code here
+  let result = [];
+  let currentSubarray = [];
+
+  for (const num of arr) {
+    if (currentSubarray.reduce((sum, val) => sum + val, 0) + num <= n) {
+      currentSubarray.push(num);
+    } else {
+      result.push(currentSubarray);
+      currentSubarray = [num]; // Start a new subarray
+    }
+  }  
+
+  result.push(currentSubarray); // Add the last subarray
+
+  return result.filter(subarr => subarr.length > 0); // Filter out empty subarrays
 };
 
+const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+
+alert(JSON.stringify(divide(arr, parseInt(n))));
